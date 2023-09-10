70f499434cefa998f7522cdb31a674a6564645da
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-09-09 20:45:30 -0500

    fixed locations on board

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
     const boardButtons = document.querySelectorAll('button')
     const gameBoard = document.querySelector('.gameBoard')
 
+    const lettersArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
     for(let i = 0; i < boardButtons.length; i++) {
         boardButtons[i].addEventListener('click', (e) => {
             createBoard(e.target.value)
@@ -12,7 +14,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
         for(let i = 0; i < e; i++) {
             for(let j = 0; j < e; j++) {
                 const newSquare = document.createElement('div')
-                newSquare.setAttribute('row-id', i + 1)
+                newSquare.setAttribute('row-id', lettersArr[i])
                 newSquare.setAttribute('square-value', j + 1);  
                 newSquare.classList.add('gameSquare'); 
                 gameBoard.append(newSquare)
@@ -25,30 +27,31 @@ document.addEventListener('DOMContentLoaded', (e) => {
         const spaces = document.querySelectorAll('.gameSquare')
         for(let i = 0; i < spaces.length; i++) {
             const rowId = spaces[i].getAttribute('row-id')
-            const squareValue = spaces[i].getAttribute('square-value')
-            if(rowId === "1") {
-                const topLeft = document.createElement('div')
-                topLeft.classList.add('topLeft')
-                const bottomLeft = document.createElement('div')
-                bottomLeft.classList.add('bottomLeft')
-                spaces[i].append(topLeft, bottomLeft)
-                if(squareValue === "9") {
-                    const topRight = document.createElement('div')
-                    topRight.classList.add('topRight')
-                    const bottomRight = document.createElement('div')
-                    bottomRight.classList.add('bottomRight')
-                    spaces[i].append(topRight, bottomRight)
-                }
+            const squareValue = parseInt(spaces[i].getAttribute('square-value'))
+            const topLeft = document.createElement('div')
+            topLeft.classList.add('topLeft')
+            spaces[i].append(topLeft)
+            topLeft.setAttribute('location', topLeft.parentElement.getAttribute('row-id') + squareValue)
+            if(squareValue === 9) {
+                const topRight = document.createElement('div')
+                topRight.classList.add('topRight')
+                // const bottomRight = document.createElement('div')
+                // bottomRight.classList.add('bottomRight')
+                spaces[i].append(topRight)
+                topRight.setAttribute('location', topRight.parentElement.getAttribute('row-id') + (squareValue + 1) )
+                // bottomRight.setAttribute('location', bottomRight.parentElement.getAttribute('row-id') + squareValue)
             }
-            else {
+            if(rowId === 'i') {
                 const bottomLeft = document.createElement('div')
                 bottomLeft.classList.add('bottomLeft')
                 spaces[i].append(bottomLeft)
-                if(squareValue === "9") {
+                if(squareValue === 9) {
                     const bottomRight = document.createElement('div')
                     bottomRight.classList.add('bottomRight')
                     spaces[i].append(bottomRight)
+                    bottomRight.setAttribute('location', lettersArr[lettersArr.length - 1] + (squareValue + 1))
                 }
+                bottomLeft.setAttribute('location', lettersArr[lettersArr.length - 1] + squareValue)
             }
             
         }
